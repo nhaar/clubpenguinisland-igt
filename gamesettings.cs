702793786c8fc79a2f35b93e1e8140a4b3aa6dd7
nhaar <38634785+nhaar@users.noreply.github.com>
2023-03-06 09:26:10 -0300

    Autosplitter related change

--- gamesettings.cs
+++ gamesettings.cs
@@ -54,7 +54,7 @@ public void ResetRun()
 	ZoneTransitionService.isRunFinished = false;
 	ZoneTransitionService.saveData("running", "False");
 	ZoneTransitionService.saveData("ended", "False");
-	ZoneTransitionService.toCheatEngine3 = 111;
+	ZoneTransitionService.toCheatEngine2 = 999;
 }
 
 //Display Timer Settings
